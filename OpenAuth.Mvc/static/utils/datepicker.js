245a1ea0a566c5a63cec8e35a45a094e76720e60
net_zh110@126.com <net_zh110@126.com>
2019-02-01 12:30:36 +0800

    重新整理

---
+++ OpenAuth.Mvc/static/utils/datepicker.js
@@ -0,0 +1,31 @@
+﻿/**
+ * 初始化 datepicker 控件
+ * 
+ * */
+var adminLteDatepicker = function () {
+
+    var initDatepicker = function (opts) {
+
+        opts = opts || {};
+        opts.class = opts.class || "adminlte_datepicker";
+        opts.autoclose = opts.autoclose || true;
+        opts.language = opts.language || "zh-CN";
+        opts.format = opts.format || "yyyy-mm-dd";
+        opts.callback = opts.callback || function (ev) { }
+
+        $('.' + opts.class).datepicker({
+            language: opts.language,
+            autoclose: opts.autoclose,
+            format: opts.format
+        }).on('changeDate', function (ev) {
+            opts.callback.call(this, ev);
+        });
+    }
+
+    return {
+
+        init: function (opts) {
+            initDatepicker(opts);
+        }
+    }
+}();
---
+++ OpenAuth.Mvc/static/utils/validate.js
@@ -0,0 +1,105 @@
+/**
+ * 函数对象
+ */
+var Validate = function () {
+
+
+    var formObj = {};
+
+    /**
+     * 初始化 jquery validation
+     */
+    var handlerInitValidate = function () {
+        formObj =  $("#inputForm").validate({
+            errorElement: 'span',
+            errorClass: 'help-block',
+
+            errorPlacement: function (error, element) {
+                element.parent().parent().attr("class", "form-group has-error");
+                error.insertAfter(element);
+            }
+        });
+    };
+
+    /**
+     * 初始化 jquery validation 指定表单提交回调函数
+     * @param {any} submitHandler
+     */
+    var handlerInitValidate = function (submitHandler) {
+
+        formObj =  $("#inputForm").validate({
+            errorElement: 'span',
+            errorClass: 'help-block',
+            errorPlacement: function (error, element) {
+                element.parent().parent().attr("class", "form-group has-error");
+                error.insertAfter(element);
+            }, 
+            /**
+             * 验证成功
+             * @param {any} error
+             * @param {any} element
+             */
+            success: function (error, element) {
+     
+                $(element).parent().parent().attr("class", "form-group has-success");                
+            },
+            /**
+             * 显示错误
+             * @param {any} error
+             * @param {any} errorList
+             */
+            showErrors: function (error, errorList) {
+                $.each(errorList, function (idx, item) {
+                    $(item.element).parent().parent().attr("class", "form-group has-error");
+                });
+                formObj.defaultShowErrors();
+            },
+            submitHandler: function (form) {
+                if (formObj.form()) {
+                    submitHandler.call(this);
+                }
+            }
+        });
+
+    }
+
+    /**
+     * 增加自定义验证规则
+     */
+    var handlerInitCustomValidate = function () {
+        $.validator.addMethod("mobile", function (value, element) {
+            var length = value.length;
+            var mobile = /^(((13[0-9]{1})|(15[0-9]{1}))+\d{8})$/;
+            return this.optional(element) || (length == 11 && mobile.test(value));
+        }, "手机号码格式错误");
+    };
+
+    return {
+        /**
+         * 初始化
+         */
+        init: function () {
+            handlerInitCustomValidate();
+            handlerInitValidate();
+        },
+        /**
+         * 初始化 指定表单提交回调函数
+         * @param {any} submitHandler
+         */
+        init: function (submitHandler) {
+            handlerInitCustomValidate();
+            handlerInitValidate(submitHandler);
+        },
+        /**
+         *  验证 
+         * */
+        valid: function () {
+            if (!$.isEmptyObject(formObj)) {
+                $("#inputForm").submit();
+                return formObj.form();
+            }
+            return false;
+        }
+    }
+
+}();
