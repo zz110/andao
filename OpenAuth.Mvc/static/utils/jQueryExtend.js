d0baa543e2e906d43e537ae9357b834f5952392f
net_zh110@126.com <net_zh110@126.com>
2019-02-06 21:38:49 +0800

    读取配置的菜单

---
+++ OpenAuth.Mvc/static/utils/jQueryExtend.js
@@ -0,0 +1,29 @@
+﻿
+(function ($) {
+
+    $.fn.extend({
+        loadMenus: function (modulecode) {
+            var dom = $(this);
+            $.ajax("/ModuleManager/LoadAuthorizedMenus?modulecode=" + modulecode,
+                {
+                    async: false
+                    , success: function (data) {
+                        if ('' !== data) {
+                            var obj = JSON.parse(data);
+                            var sb = '';
+                            $.each(obj,
+                                function () {
+                                    var element = this;
+                                    sb += "<button class='btn " + element.Class + "' id='" + element.DomId + "'>" + element.Name + "</button> ";
+                                });
+                            dom.html(sb);
+                        }
+                        else {
+                            $(this).hide();
+                        }
+                    }
+                });
+        }
+    });
+
+})(jQuery);
