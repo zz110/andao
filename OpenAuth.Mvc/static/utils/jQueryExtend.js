1263533be13b839875f753e7ba1e80025179a314
net_zh110@126.com <net_zh110@126.com>
2019-02-07 09:05:17 +0800

    完成操作按钮加载

--- OpenAuth.Mvc/static/utils/jQueryExtend.js
+++ OpenAuth.Mvc/static/utils/jQueryExtend.js
@@ -4,11 +4,13 @@
     $.fn.extend({
         loadMenus: function (modulecode) {
             var dom = $(this);
+            var flag = false;
             $.ajax("/ModuleManager/LoadAuthorizedMenus?modulecode=" + modulecode,
                 {
                     async: false
                     , success: function (data) {
-                        if ('' !== data) {
+                        
+                        if ('' !== data && '[]' !== data) {
                             var obj = JSON.parse(data);
                             var sb = '';
                             $.each(obj,
@@ -17,12 +19,14 @@
                                     sb += "<button class='btn " + element.Class + "' id='" + element.DomId + "'>" + element.Name + "</button> ";
                                 });
                             dom.html(sb);
-                        }
-                        else {
-                            $(this).hide();
+                            flag = true;
                         }
                     }
                 });
+
+            if (!flag) {
+                $(this).css('display', 'none');
+            }
         }
     });
 
