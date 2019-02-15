25b1ed38dc61351833f0b0a5f7b6127d50bbba53
net_zh110@126.com <net_zh110@126.com>
2019-02-15 11:26:44 +0800

    Judge

---
+++ OpenAuth.Mvc/Controllers/JudgeController.cs
@@ -0,0 +1,20 @@
+﻿using OpenAuth.App.Response;
+using OpenAuth.App.SSO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OpenAuth.Mvc.Controllers
+{
+    public class JudgeController : Controller
+    {
+        // GET: Judge
+        public ActionResult Index()
+        {
+            ViewBag.user = AuthUtil.GetCurrentUser().User;
+            return View();
+        }
+    }
+}
