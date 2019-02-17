13591a4a9a85e2d7f5f29e30d51a133e8bcae7c9
sch <sch@LAPTOP-8HB0BBG2>
2019-02-17 14:50:41 +0800

    111

--- OpenAuth.App/PerformanceAppraisalApp.cs
+++ OpenAuth.App/PerformanceAppraisalApp.cs
@@ -23,5 +23,84 @@ namespace OpenAuth.App
                 rows = rows
             };
         }
+        public List<PerformanceAppraisalOutPut> List(string year, PerformanceAppraisalQueryInput input)
+        {
+            string sql = $@"select top 1000 JudgeId,JudgeName,
+                            (select SUM(Score)/12 from MonthlyAssessment 
+                            where UserId = JudgeId 
+                            and EvaluateYear='{ year }') 
+                            MonthlyAVG,
+                            sum(
+	                            case q1 
+	                            when 10 then 1 
+	                            when 11 then 0.7 
+	                            when 12 then 0.3 
+	                            else 0 
+	                            end
+                            ) q1,
+                            sum(
+	                            case q2 
+	                            when 10 then 1 
+	                            when 11 then 0.7 
+	                            when 12 then 0.3 
+	                            else 0 
+	                            end
+                            ) q2,
+                            sum(
+	                            case q3 
+	                            when 10 then 1 
+	                            when 11 then 0.7 
+	                            when 12 then 0.3 
+	                            else 0 
+	                            end
+                            ) q3,
+                            sum(
+	                            case q4 
+	                            when 10 then 1 
+	                            when 11 then 0.7 
+	                            when 12 then 0.3 
+	                            else 0 
+	                            end
+                            ) q4,
+                            sum(
+	                            case q5 
+	                            when 10 then 1 
+	                            when 11 then 0.7 
+	                            when 12 then 0.3 
+	                            else 0 
+	                            end
+                            ) q5,
+                            sum(
+	                            case q6
+	                            when 10 then 1 
+	                            when 11 then 0.8 
+	                            when 12 then 0.6 
+	                            when 13 then 0.3 
+	                            else 0 
+	                            end
+                            ) q6,
+                            sum(
+	                            case 
+	                            when q6 is not null then 1 
+	                            else 0 
+	                            end
+                            ) q6count,
+                            (select AccessmentScore from PerformanceAppraisal 
+                            where JudgeId = JudgeId) AccessmentScore 
+                            from(
+                              select row_number() over(order by a.Optime) as num,
+                              a.RatersId,a.JudgeId,uj.Name as RatersName,ur.Name as JudgeName,
+                              a.q1,a.q2,a.q3,a.q4,a.q5,a.q6 from Answer a 
+                              inner join [user] ur on ur.Id = a.JudgeId 
+                              inner join [user] uj on uj.Id = a.RatersId 
+                            ) as t 
+                            left join Relevance r on r.FirstId = t.JudgeId 
+                            inner join [Role] ro on ro.Id = r.SecondId 
+                            where num > 0 and ro.Name = '正职' 
+                            group by JudgeId,JudgeName";
+            var rows = Repository.ExecuteQuerySql<PerformanceAppraisalOutPut>(sql, input.ToParameters()).ToList();
+
+            return rows;
+        }
     }
 }
--- OpenAuth.Mvc/Controllers/CheckAndEvaluateController.cs
+++ OpenAuth.Mvc/Controllers/CheckAndEvaluateController.cs
@@ -19,10 +19,29 @@ namespace OpenAuth.Mvc.Controllers
         }
 
         [System.Web.Mvc.HttpGet]
-        public ActionResult List(int limit, int offset, PerformanceAppraisalQueryInput input)
+        //int limit, int offset, PerformanceAppraisalQueryInput input
+        public ActionResult List(string year,string type)
         {
-            var result = App.page(limit, offset, input);
-            return Json(result, JsonRequestBehavior.AllowGet);
+            PerformanceAppraisalQueryInput input = new PerformanceAppraisalQueryInput();
+            //var result = App.List("970edfb5-53c7-4dc8-a086-08a28ba4686a", "2019", input);
+            var result = App.List(year, input);
+            List<PerformanceAppraisalOutPut> list = result as List<PerformanceAppraisalOutPut>;
+            for (int i = 0; i < list.Count; i++)
+            {
+                list[i].q6 = list[i].q6 * 100 / list[i].q6count;
+                if (type == "正职")
+                {
+                    list[i].pingce = (list[i].q1 + list[i].q2 + list[i].q3 + list[i].q4 +
+                        list[i].q5) * (decimal)0.6 + list[i].q6 * (decimal)0.4;
+                }
+                else
+                {
+                    list[i].pingce = list[i].q6;
+                }
+                list[i].kaopingdefen = list[i].pingce * (decimal)0.4 +
+                    (list[i].MonthlyAVG + list[i].AccessmentScore) * (decimal)0.3;
+            }
+            return Json(new { total = 10000, rows = result }, JsonRequestBehavior.AllowGet);
         }
     }
 }
